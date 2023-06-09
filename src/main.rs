a159c54c3a50c46cf5973f63665be22d7ebe4f99
e6quisitory <mhamzaqayyum@gmail.com>
2023-06-09 09:02:33 -0700

    started lvalue operators

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 mod utils;
-use utils::utils::Vec2D;
-use utils::utils::Vec2;
+use utils::vec2d::Vec2D;
+use utils::vec2d::Vec2;
 
 fn main() {
 
     let v1: Vec2 = Vec2D::new(1.0,2.8);
     let v2: Vec2 = Vec2D::new(1.5, -2.0);
-    let v3 = v1 - v2;
+    let mut v3 = v1 - v2;
+
+    let e1 = v3[0];
 
     println!("{:?}", v3);
     println!("{}", Vec2D::new(1,1) == Vec2D::new(0,1));
+    println!("{}", e1);
+
+    v3[0] = 24.3;
+
+    println!("{:?}", v3);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,31 @@
-pub mod utils {
-    use std::ops::{Add, Sub, AddAssign, SubAssign};
+pub mod vec2d {
+    use std::ops::{Add, Sub, AddAssign, SubAssign, Index, IndexMut};
 
-    //#[derive(Copy)]
+    /*
+    =================================================
+        Trait bounds for a valid Vec2D element
+    =================================================
+    */
+
+    pub trait ValidVecElement: Copy + Add<Output = Self> + Sub<Output = Self> {}
+    impl<T: Copy + Add<Output = Self> + Sub<Output = Self>> ValidVecElement for T {}
+
+    /*
+    =================================================
+        Vec2D struct definition
+    =================================================
+    */
+
     #[derive(Debug, PartialEq)]
-    pub struct Vec2D<T: Copy + Add<Output = T> + Sub<Output = T>> {
+    pub struct Vec2D<T: ValidVecElement> {
         pub e: [T; 2]
     }
+
+    /*
+    =================================================
+        Type aliases for Vec2D
+    =================================================
+    */
 
     pub type Vec2    = Vec2D<f64>;
     pub type iVec2   = Vec2D<i32>;
@@ -13,13 +33,46 @@ pub mod utils {
     pub type iPoint2 = iVec2;
     pub type Pixel   = iVec2;
 
-    impl<T: Copy + Add<Output = T> + Sub<Output = T>> Vec2D<T> {
+    /*
+    =================================================
+        Vec2D methods
+    =================================================
+    */
+
+    impl<T: ValidVecElement> Vec2D<T> {
         pub fn new(e1: T, e2: T) -> Self {
             return Vec2D { e: [e1, e2] }
         }
     }
 
-    impl<T: Copy + Add<Output = T> + Sub<Output = T>> Add for Vec2D<T> {
+    /*
+    =================================================
+        lvalue operators
+    =================================================
+    */
+
+    impl<T: ValidVecElement> Index<usize> for Vec2D<T> {
+        type Output = T;
+
+        fn index(&self, index: usize) -> &Self::Output {
+            return &self.e[index];
+        }
+    }
+
+    impl<T: ValidVecElement> IndexMut<usize> for Vec2D<T> {
+        fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+            return &mut self.e[index];
+        }
+    }
+
+
+    /*
+    =================================================
+        rvalue operators
+    =================================================
+    */
+
+    impl<T: ValidVecElement> Add for Vec2D<T> {
         type Output = Self;
 
         fn add(self, rhs: Self) -> Self::Output {
@@ -27,7 +80,7 @@ pub mod utils {
         }
     }
 
-    impl<T: Copy + Add<Output = T> + Sub<Output = T>> Sub for Vec2D<T> {
+    impl<T: ValidVecElement> Sub for Vec2D<T> {
         type Output = Self;
 
         fn sub(self, rhs: Self) -> Self::Output {
