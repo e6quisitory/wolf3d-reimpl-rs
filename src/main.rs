a545e0f76bbd5a55439d0bb593ed098c11c72032
e6quisitory <mhamzaqayyum@gmail.com>
2023-06-09 13:18:57 -0700

    2D vector library done

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 mod utils;
-use utils::vec2d::Vec2D;
-use utils::vec2d::Vec2;
+use utils::vec2d::*;
 
 fn main() {
 
-    let v1: Vec2 = Vec2D::new(1.0,2.8);
-    let v2: Vec2 = Vec2D::new(1.5, -2.0);
+    let v1 = Vec2D::New(1.0,2.8);
+    let v2 = Vec2D::New(1.5, -2.0);
     let mut v3 = v1 - v2;
 
     let e1 = v3[0];
 
     println!("{:?}", v3);
-    println!("{}", Vec2D::new(1,1) == Vec2D::new(0,1));
+    println!("{}", Vec2D::New(1,1) == Vec2D::New(0,1));
     println!("{}", e1);
 
     v3[0] = 24.3;
 
     println!("{:?}", v3);
+
+    let v6 = Vec2D::New(1.0, 2.0);
+    let v7 = Vec2D::New(3.0, 4.0);
+    let v8 = v7 - v6;
+    println!("{:?}", v8.x());
+    println!("{:?}", v8.y());
+    println!("{:?}", v8);
+    println!("{:?}", v8.LengthSquared());
+    println!("{:?}", v8.Length());
+    println!("{:?}", v8.Rotate(3.14159/2.0));
+    println!("{:?}", v8.UnitVector());
+    println!("{:?}", Dot(&v3, &v8));
+    println!("{:?}", -v8);
+
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
 pub mod vec2d {
-    use std::ops::{Add, Sub, AddAssign, SubAssign, Index, IndexMut};
+    use std::ops::{Add, Sub, Div, AddAssign, SubAssign, Index, IndexMut, Neg};
 
     /*
     =================================================
@@ -7,8 +7,8 @@ pub mod vec2d {
     =================================================
     */
 
-    pub trait ValidVecElement: Copy + Add<Output = Self> + Sub<Output = Self> {}
-    impl<T: Copy + Add<Output = Self> + Sub<Output = Self>> ValidVecElement for T {}
+    pub trait ValidVecElement: Copy + Add<Output = Self> + Sub<Output = Self> + AddAssign + SubAssign + Neg<Output = Self> {}
+    impl<T: Copy + Add<Output = Self> + Sub<Output = Self> + AddAssign + SubAssign + Neg<Output = Self>> ValidVecElement for T {}
 
     /*
     =================================================
@@ -40,8 +40,35 @@ pub mod vec2d {
     */
 
     impl<T: ValidVecElement> Vec2D<T> {
-        pub fn new(e1: T, e2: T) -> Self {
+        pub fn New(e1: T, e2: T) -> Self {
             return Vec2D { e: [e1, e2] }
+        }
+
+        pub fn x(&self) -> T {
+            return self.e[0];
+        }
+
+        pub fn y(&self) -> T {
+            return self.e[1];
+        }
+    }
+
+    impl Vec2D<f64> {
+        pub fn LengthSquared(&self) -> f64 {
+            return self.e[1].powi(2) + self.e[1].powi(2);
+        }
+
+        pub fn Length(&self) -> f64 {
+            return self.LengthSquared().sqrt();
+        }
+
+        pub fn Rotate(&self, rad: f64) -> Self {
+            return Vec2D::New(self.e[0]*rad.cos() - self.e[1]*rad.sin(), self.e[0]*rad.sin() + self.e[1]*rad.cos());
+        }
+
+        pub fn UnitVector(&self) -> Self {
+            let vectorMag = self.Length();
+            return Vec2D::New(self.e[0]/vectorMag, self.e[1]/vectorMag);
         }
     }
 
@@ -65,6 +92,27 @@ pub mod vec2d {
         }
     }
 
+    impl<T: ValidVecElement> AddAssign for Vec2D<T> {
+        fn add_assign(&mut self, rhs: Self) {
+            self.e[0] += rhs[0];
+            self.e[1] += rhs[1];
+        }
+    }
+
+    impl<T: ValidVecElement> SubAssign for Vec2D<T> {
+        fn sub_assign(&mut self, rhs: Self) {
+            self.e[0] -= rhs[0];
+            self.e[1] -= rhs[1];
+        }
+    }
+
+    impl<T: ValidVecElement> Neg for Vec2D<T> {
+        type Output = Self;
+
+        fn neg(self) -> Self::Output {
+            return Vec2D::New(-self.e[0], - self.e[1]);
+        }
+    }
 
     /*
     =================================================
@@ -76,7 +124,7 @@ pub mod vec2d {
         type Output = Self;
 
         fn add(self, rhs: Self) -> Self::Output {
-            return Vec2D::new(self.e[0] + rhs.e[0], self.e[1] + rhs.e[1]);
+            return Vec2D::New(self.e[0] + rhs.e[0], self.e[1] + rhs.e[1]);
         }
     }
 
@@ -84,7 +132,18 @@ pub mod vec2d {
         type Output = Self;
 
         fn sub(self, rhs: Self) -> Self::Output {
-            return Vec2D::new(self.e[0] - rhs.e[0], self.e[1] - rhs.e[1]);
+            return Vec2D::New(self.e[0] - rhs.e[0], self.e[1] - rhs.e[1]);
         }
     }
+
+    /*
+    =================================================
+        Free functions
+    =================================================
+    */
+
+    pub fn Dot(v1: &Vec2D<f64>, v2: &Vec2D<f64>) -> f64 {
+        return v1.x()*v2.x() + v1.y()*v2.y();
+    }
+
 }
