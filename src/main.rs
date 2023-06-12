4f298ebbf811fc98b29098a615ed2105594d552d
e6quisitory <mhamzaqayyum@gmail.com>
2023-06-12 11:49:51 -0700

    csv reader for map file

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
 mod utils;
-use utils::vec2d::*;
+
+use ndarray::Array2;
+use utils::mapCSV::*;
 
 fn main() {
+    let array = match parseCSV("map.csv") {
+        Ok(array) => array,
+        Err(err) => {
+            println!("Error: {}", err);
+            return;
+        },
+    };
 
-    let v1 = Vec2D::New(1.0,2.8);
-    let v2 = Vec2D::New(1.5, -2.0);
-    let mut v3 = v1 - v2;
-
-    let e1 = v3[0];
-
-    println!("{:?}", v3);
-    println!("{}", Vec2D::New(1,1) == Vec2D::New(0,1));
-    println!("{}", e1);
-
-    v3[0] = 24.3;
-
-    println!("{:?}", v3);
-
-    let v6 = Vec2D::New(1.0, 2.0);
-    let v7 = Vec2D::New(3.0, 4.0);
-    let v8 = v7 - v6;
-    println!("{:?}", v8.x());
-    println!("{:?}", v8.y());
-    println!("{:?}", v8);
-    println!("{:?}", v8.LengthSquared());
-    println!("{:?}", v8.Length());
-    println!("{:?}", v8.Rotate(3.14159/2.0));
-    println!("{:?}", v8.UnitVector());
-    println!("{:?}", Dot(&v3, &v8));
-    println!("{:?}", -v8);
-
-    let p = Point2::New(1.5, -9.5);
-    let ip: iPoint2 = iPoint2::from(p);
-    println!("{:?}", ip);
-
+    println!("{:?}", &array);
+    println!("{}, {}", array.nrows(), array.ncols());
 }
--- src/utils.rs
+++ src/utils.rs
@@ -500,3 +500,73 @@ pub mod dda {
     }
 
 }
+
+pub(crate) mod mapCSV {
+    use std::error::Error;
+    use std::fs::File;
+
+    use csv::ReaderBuilder;
+    use ndarray::Array2;
+
+    pub fn parseCSV(path: &str) -> Result<Array2<i32>, Box<dyn Error>> {
+        // Open the file
+        let file = File::open(path)?;
+
+        // Build CSV reader with ',' as delimiter and flexible number of fields
+        let mut rdr = ReaderBuilder::new()
+            .delimiter(b',')
+            .flexible(true)
+            .has_headers(false)  // Do not treat first row as headers
+            .from_reader(file);
+
+        let mut array_data = Vec::new();
+
+        // Read each record
+        for result in rdr.records() {
+            let record = result?;
+
+            // Skip empty rows
+            if record.iter().next().is_none() {
+                continue;
+            }
+
+            let mut row = Vec::new();
+            for field in record.iter() {
+                // Convert each field to an integer, using 0 if the field is empty
+                let value: i32 = field.parse().unwrap_or(0);
+                row.push(value);
+            }
+
+            array_data.push(row);
+        }
+
+        // Find maximum row length to handle jagged arrays
+        let max_len = array_data.iter().map(|row| row.len()).max().unwrap_or(0);
+
+        // Normalize rows to have equal length
+        for row in &mut array_data {
+            let diff = max_len - row.len();
+            if diff > 0 {
+                row.extend(vec![0; diff]);
+            }
+        }
+
+        // Convert the data to a 2D array
+        let mut array: Array2<i32> = ndarray::Array::from_shape_vec((array_data.len(), max_len), array_data.concat())
+            .expect("Error converting to 2D array");
+
+        // Adjust perimeter values to be 1 if they are 0
+        let (max_i, max_j) = (array.nrows() - 1, array.ncols() - 1);
+        for i in 0..=max_i {
+            for j in 0..=max_j {
+                if i == 0 || j == 0 || i == max_i || j == max_j {
+                    if array[[i, j]] == 0 {
+                        array[[i, j]] = 1;
+                    }
+                }
+            }
+        }
+
+        Ok(array)
+    }
+}
