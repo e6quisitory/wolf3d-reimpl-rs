02e7a7db59b354498461eec6250b2e271695b990
e6quisitory <mhamzaqayyum@gmail.com>
2023-06-20 17:08:16 -0700

    working raycasting & controls

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 mod utils;
 
-use std::ops::Mul;
-use std::time::Duration;
-use ndarray::Array2;
 use utils::mapCSV::*;
-use sdl2::*;
-use sdl2::rect::{Point, Rect};
+use sdl2::rect::Rect;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
-use sdl2::image::{LoadTexture, Sdl2ImageContext};
-use sdl2::keyboard::Keycode;
-use sdl2::render::{TextureCreator, WindowCanvas};
-use sdl2::video::{Window, WindowContext};
+use sdl2::keyboard::{Keycode, Scancode};
+use sdl2::video::Window;
 use std::error::Error;
 use crate::utils::conventions::PI;
 use crate::utils::dda::{RayCursor, wallType_t};
 use crate::utils::misc_math::DegreesToRadians;
 use crate::utils::ray::Ray;
-use crate::utils::vec2d::{Dot, Point2};
+use crate::utils::vec2d::Point2;
 
 pub struct SdlContext {
     context: sdl2::Sdl,
@@ -66,13 +60,13 @@ impl Game {
 
 fn main() {
     let context = SdlContext::new().unwrap();
-    let mut game = Game::new(context).unwrap();
+    let game = Game::new(context).unwrap();
 
-    let mut canvas = game.window.into_canvas().build().unwrap();
-    let texture_creator = canvas.texture_creator();
+    let mut canvas = game.window.into_canvas().accelerated().present_vsync().build().unwrap();
+    //let texture_creator = canvas.texture_creator();
 
     // Load the texture
-    let texture = texture_creator.load_texture("SS.png").unwrap();
+    //let texture = texture_creator.load_texture("SS.png").unwrap();
 
     // Load map
     let array = match parseCSV("map.csv") {
@@ -87,19 +81,19 @@ fn main() {
     println!("{}", array.get((1,1)).unwrap());
 
     // Window params
-    const windowWidth: usize = 1280;
-    const windowHeight: usize = 720;
-    let fov: f64 = DegreesToRadians(80.0);
+    const WINDOW_WIDTH: usize = 1280;
+    const WINDOW_HEIGHT: usize = 720;
+    let fov: f64 = 90.0;
 
     // Player info
     let mut playerPos = Point2::New(4.127, 5.033);
     let mut playerViewDir = Point2::New(-0.019038625821465295, 0.7068504302374231).UnitVector();
 
     //Pre-calculate angles
-    let mut castingRayAngles: [(f64, f64); windowWidth] = [(0.0, 0.0); windowWidth];
+    let mut castingRayAngles: [(f64, f64); WINDOW_WIDTH] = [(0.0, 0.0); WINDOW_WIDTH];
     let projectionPlaneWidth: f64 = 2.0 * DegreesToRadians(fov / 2.0).tan();
-    let segmentLength: f64 = projectionPlaneWidth / windowWidth as f64;
-    for x in 0..windowWidth-1 {
+    let segmentLength: f64 = projectionPlaneWidth / WINDOW_WIDTH as f64;
+    for x in 0..WINDOW_WIDTH -1 {
         let currAngle = (-(x as f64 * segmentLength - (projectionPlaneWidth / 2.0))).atan();
         castingRayAngles[x] = (currAngle, currAngle.cos());
     }
@@ -107,42 +101,69 @@ fn main() {
     let mut event_pump = game.context.context.event_pump().unwrap();
 
     'running: loop {
-        let playerEast = playerViewDir.Rotate(PI/2.0);
-        let playerWest = playerViewDir.Rotate(-PI/2.0);
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} => { break 'running; },
-                Event::KeyDown { keycode: Some(Keycode::Left), .. } => { playerViewDir = playerViewDir.Rotate((PI/20.0)*0.15); },
-                Event::KeyDown { keycode: Some(Keycode::Right), .. } => { playerViewDir = playerViewDir.Rotate((-PI/20.0)*0.15); },
-                Event::KeyDown { keycode: Some(Keycode::W), .. } => { playerPos = playerPos + playerViewDir*0.3*0.15; },
-                Event::KeyDown { keycode: Some(Keycode::S), .. } => { playerPos = playerPos - playerViewDir*0.3*0.15; },
-                Event::KeyDown { keycode: Some(Keycode::A), .. } => { playerPos = playerPos + playerEast*0.3*0.15; },
-                Event::KeyDown { keycode: Some(Keycode::D), .. } => { playerPos = playerPos + playerWest*0.3*0.15; },
-
-
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;
+                },
                 _ => {}
             }
+        }
+
+        let playerEast = playerViewDir.Rotate(-PI/2.0);
+        let playerWest = playerViewDir.Rotate(PI/2.0);
+
+        let swivelSpeed = 0.25;
+        let moveSpeed = 0.1;
+
+        let ks = event_pump.keyboard_state();
+        let LEFT = ks.is_scancode_pressed(Scancode::Left);
+        let RIGHT = ks.is_scancode_pressed(Scancode::Right);
+        let W = ks.is_scancode_pressed(Scancode::W);
+        let S = ks.is_scancode_pressed(Scancode::S);
+        let A = ks.is_scancode_pressed(Scancode::A);
+        let D = ks.is_scancode_pressed(Scancode::D);
+
+        if W && A {
+            playerPos = playerPos + playerViewDir*moveSpeed*0.7071067 + playerWest*moveSpeed*0.7071067;
+        } else if W && D {
+            playerPos = playerPos + playerViewDir*moveSpeed*0.7071067 + playerEast*moveSpeed*0.7071067;
+        } else if W {
+            playerPos = playerPos + playerViewDir*moveSpeed;
+        } else if S {
+            playerPos = playerPos - playerViewDir*moveSpeed;
+        } else if A {
+            playerPos = playerPos + playerWest*moveSpeed;
+        } else if D {
+            playerPos = playerPos + playerEast*moveSpeed;
+        }
+
+        if LEFT {
+            playerViewDir = playerViewDir.Rotate((PI/20.0)*swivelSpeed);
+        } else if RIGHT {
+            playerViewDir = playerViewDir.Rotate((-PI/20.0)*swivelSpeed);
         }
 
         canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
         canvas.clear();
 
-        for x in 0..windowWidth-1 {
-            let mut currRay = Ray::New(playerPos, playerViewDir.Rotate(castingRayAngles[x].0));
+        for x in 0..WINDOW_WIDTH -1 {
+            let currRay = Ray::New(playerPos, playerViewDir.Rotate(castingRayAngles[x].0));
             let mut rayCursor = RayCursor::New(currRay, playerPos);
             while (rayCursor.hitTile.x() >= 0 && rayCursor.hitTile.x() < mapWidth as i32) && (rayCursor.hitTile.y() >= 0 && rayCursor.hitTile.y() < mapHeight as i32) {
                 rayCursor.GoToNextHit();
-                if *(array.get((rayCursor.hitusTile.x() as usize, rayCursor.hitTile.y() as usize)).unwrap()) == 1 {
+                if *(array.get((rayCursor.hitTile.x() as usize, rayCursor.hitTile.y() as usize)).unwrap()) == 1 {
                     let dist = rayCursor.GetDistToHitPoint();
-                    let renderHeight = (1000.0/(dist*castingRayAngles[x].1)) as usize;
-                    if (rayCursor.GetWallType() == wallType_t::VERTICAL) {
-                        canvas.set_draw_color(Color::RGBA(255, 0, 0, 255));
+                    let renderHeight = (400.0/(dist*castingRayAngles[x].1)) as usize;
+                    if rayCursor.GetWallType() == wallType_t::VERTICAL {
+                        canvas.set_draw_color(Color::RGBA(199, 199, 199, 255));
                     } else {
-                        canvas.set_draw_color(Color::RGBA(0, 0, 255, 255));
+                        canvas.set_draw_color(Color::RGBA(81, 81, 81, 255));
                     }
 
                     //canvas.fill_rect(Rect::new(x as i32, 0, 1, 10));
-                    let mut y = ((windowHeight as f64 / 2.0) - (renderHeight as f64 / 2.0)) as i32;
+                    let y = ((WINDOW_HEIGHT as f64 / 2.0) - (renderHeight as f64 / 2.0)) as i32;
                     canvas.fill_rect(Rect::new(x as i32, y, 1, renderHeight as u32)).unwrap();
                     break;
                 }
@@ -150,11 +171,6 @@ fn main() {
         }
 
         canvas.present();
-
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-
-        println!("{:?}", playerPos);
-        println!("{:?}", playerViewDir);
     }
 }
 
